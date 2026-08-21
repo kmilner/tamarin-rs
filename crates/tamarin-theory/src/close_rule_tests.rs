f5037f47be3be300fc5deb7ca7576edd652280f2
Niklas Medinger <nik.medinger@gmail.com>
2026-08-22 01:06:27 +0200

    Bump headers

--- crates/tamarin-theory/src/close_rule_tests.rs
+++ crates/tamarin-theory/src/close_rule_tests.rs
@@ -1,3 +1,7 @@
+// Currently GPL 3.0 until granted permission by the upstream authors
+// of the tamarin-prover sources this file cites; list them with:
+//   scripts/gen_license_headers.py --authors <this file>
+
 use super::*;
 use crate::fact::{apply_subst_fact, Fact};
 use tamarin_parser::{Formula, DUMMY_LOCATION};
