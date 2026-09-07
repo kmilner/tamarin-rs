e6c510d03b9a6a5284beef8dfe4bdf5b37de990f
Kevin Milner <kamilner@kamilner.ca>
2026-09-07 22:18:02 +0100

    Refresh CI oracle certification and generated licence headers

--- crates/tamarin-server/src/theory_io_auto_sources_tests.rs
+++ crates/tamarin-server/src/theory_io_auto_sources_tests.rs
@@ -1,7 +1,3 @@
-// Currently GPL 3.0 until granted permission by the upstream authors
-// of the tamarin-prover sources this file cites; list them with:
-//   scripts/gen_license_headers.py --authors <this file>
-
 use super::*;
 use tamarin_test_support::require_maude_path;
 
